1ea6da09f37133dcbecb6c514637073db3b1f38c
Vincent Robert <vincent.robert@nanego.com>
2020-07-20 13:54:26 +0200

    Add new feature to README file

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 This plugin adds some small features we are testing and plan to commit to Redmine core if validated.
 
 Here is a complete list of the features:
-* adds a warning message when a user is editing a **closed** issue
+* Display a warning message when a user is editing a **closed** issue
+* Hide optional advanced fields in **version** form
--- init.rb
+++ init.rb
@@ -4,10 +4,10 @@ require 'redmine_tiny_features/hooks'
 Redmine::Plugin.register :redmine_tiny_features do
   name 'Redmine Tiny Features plugin'
   author 'Vincent ROBERT'
-  description 'This is a Redmine plugin for testing small features before committing them to Redmine core'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  description 'This is a Redmine plugin used to test small features before committing them to Redmine core'
+  version '1.0.0'
+  url 'https://github.com/nanego/redmine_tiny_features'
+  author_url 'https://github.com/nanego'
   requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.4' if Rails.env.test?
   requires_redmine_plugin :redmine_base_deface, :version_or_higher => '0.0.1'
 end
