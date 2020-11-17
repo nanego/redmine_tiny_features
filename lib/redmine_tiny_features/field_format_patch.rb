a7592429be09ac397be48dd637a6a79ce6162ed9
Vincent Robert <vincent.robert@nanego.com>
2020-11-17 15:14:49 +0100

    Fix possible_values when there is no context

--- lib/redmine_tiny_features/field_format_patch.rb
+++ lib/redmine_tiny_features/field_format_patch.rb
@@ -5,16 +5,16 @@ module Redmine
 
     class EnumerationFormat < RecordList
 
-      def possible_values_records(custom_field, object=nil)
-        disabled_ids = DisabledCustomFieldEnumeration.disabled_ids_for(object.project) if object.present? && object.try(:project).present?
-        if disabled_ids.any?
-          custom_field.enumerations.active.where("id NOT IN (?)", disabled_ids)
-        else
-          custom_field.enumerations.active
+      def possible_values_records(custom_field, object = nil)
+        enumerations = custom_field.enumerations.active
+        if object.present? && object.try(:project).present?
+          disabled_ids = DisabledCustomFieldEnumeration.disabled_ids_for(object.project)
+          enumerations = enumerations.where("id NOT IN (?)", disabled_ids) if disabled_ids.present?
         end
+        enumerations
       end
-
     end
 
   end
+
 end
