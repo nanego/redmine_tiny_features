e8faade1564335e5004b5c13ca7e6fb95ca39f5e
Vincent Robert <vincent.robert@nanego.com>
2022-09-12 16:22:49 +0200

    Complete Readme and include new enabled-modules filter and column

--- lib/redmine_tiny_features/project_patch.rb
+++ lib/redmine_tiny_features/project_patch.rb
@@ -5,7 +5,7 @@ class Project < ActiveRecord::Base
   has_many :disabled_custom_field_enumerations, :dependent => :delete_all
 
   def module_enabled    
-    EnabledModule.where("project_id = ? ", self.id).map { |e_m| l("project_module_#{e_m.name}") }
+    EnabledModule.where("project_id = ? ", self.id).order(:name).map { |e_m| l("project_module_#{e_m.name}") }
   end
 
 end
