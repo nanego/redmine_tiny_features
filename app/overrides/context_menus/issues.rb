c35d439d3f47c54133bfc9271fa9e8f0daa4a8a9
Yazan ALAEDDIN <y.alaeddin@adacis.net>
2022-10-05 14:47:51 +0200

    Ajouter la conditions @issue.present?

--- app/overrides/context_menus/issues.rb
+++ app/overrides/context_menus/issues.rb
@@ -2,5 +2,5 @@ Deface::Override.new :virtual_path => 'context_menus/issues',
                      :name         => 'hide-copy-issue-if-tracker-prevents-it',
                      :insert_bottom=> 'erb[loud]:contains("context_menu_link l(:button_copy)")',
                      :text         => <<-HIDE_LINK
-    if !@issue.tracker.prevent_copy_issues 
+    if @issue.present? && !@issue.tracker.prevent_copy_issues 
 HIDE_LINK
--- app/overrides/issues/_action_menu.rb
+++ app/overrides/issues/_action_menu.rb
@@ -2,5 +2,5 @@ Deface::Override.new :virtual_path => 'issues/_action_menu',
                      :name         => 'hide-copy-issue-if-tracker-prevents-it',
                      :insert_bottom=> 'erb[loud]:contains("link_to l(:button_copy)")',
                      :text         => <<-HIDE_LINK
-    && !@issue.tracker.prevent_copy_issues 
+    && @issue.present? && !@issue.tracker.prevent_copy_issues 
 HIDE_LINK
