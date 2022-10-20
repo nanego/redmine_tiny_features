1d129d99387febd0f280d68bf9a89089c4fc1573
Yazan ALAEDDIN <y.alaeddin@adacis.net>
2022-10-20 14:16:22 +0200

    Pouvoir désactiver la fonction de masquage des emails

--- init.rb
+++ init.rb
@@ -41,6 +41,7 @@ Redmine::Plugin.register :redmine_tiny_features do
                'default_project': '',
                'paginate_issue_filters_values': Rails.env.test? || !(Redmine::Plugin.installed?(:redmine_base_select2)) ? '0' : '1',
                'journalize_note_deletion': Rails.env.test? ? '0' : '1',
-               'use_select2': Rails.env.test? || !(Redmine::Plugin.installed?(:redmine_base_select2)) ? '0' : '1'
+               'use_select2': Rails.env.test? || !(Redmine::Plugin.installed?(:redmine_base_select2)) ? '0' : '1',
+               'disable_email_hiding': '',
            }
 end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,34 @@
+require "spec_helper"
+require "active_support/testing/assertions"
+
+describe UsersController, type: :controller do
+  include ActiveSupport::Testing::Assertions
+
+  render_views
+
+  fixtures :users, :email_addresses
+
+  before do
+    @request.session[:user_id] = 1 #=> admin ; permissions are hard...
+  end
+
+  context "Disable emails hiding function" do
+
+    it "should show user's email when the option is selected, even if the user hides his email" do
+      Setting["plugin_redmine_tiny_features"]["disable_email_hiding"] = '1'
+      expect(User.find(2).pref.hide_mail).to eq true
+      get :show, params: { :id => 2 }
+
+      expect(response.body).to include(User.find(2).mail)
+    end
+
+    it "should hide user's email when the option is unselected, if the user hides his email" do
+      Setting["plugin_redmine_tiny_features"]["disable_email_hiding"] = ''
+      expect(User.find(2).pref.hide_mail).to eq true
+      get :show, params: { :id => 2 }
+
+      expect(response.body).to_not include(User.find(2).mail)
+    end
+  end
+
+end
