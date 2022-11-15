2f4f68428dc4929dcc7e10c333f902621f9a6c2a
Yalaeddin <89978016+Yalaeddin@users.noreply.github.com>
2022-11-15 16:25:09 +0100

    Add ability to disable hidden-mails-addresses feature
    * Pouvoir désactiver la fonction de masquage des emails
    
    * Masquer  l'option "Cacher mon adresse mail" quand l'option "Désactiver la fonction de masquage des emails" est activé
    
    * Nouvelle permissions permettant de voir facilement toutes les adresses mails d'un utilisateur

---
+++ app/overrides/users/show.rb
@@ -0,0 +1,6 @@
+Deface::Override.new :virtual_path      => "users/show",
+                    :name         		=> "add-condition-disable_email_hiding",
+                    :replace      		=> "erb[silent]:contains('unless @user.pref.hide_mail')",
+                    :text         		=> <<STRING_EMAIL
+<% unless @user.pref.hide_mail && !Setting["plugin_redmine_tiny_features"]["disable_email_hiding"].present? && !User.current.allowed_to?(:always_see_user_email_addresses, nil, :global => true) %>
+STRING_EMAIL
