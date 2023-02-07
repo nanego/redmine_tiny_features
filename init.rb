44e71037727b6b8bfe96d1d7ccd56a2d6224fa65
Vincent Robert <vincent.robert@nanego.com>
2023-02-07 16:57:52 +0100

    Remove unnecessary module

--- init.rb
+++ init.rb
@@ -38,20 +38,19 @@ Redmine::Plugin.register :redmine_tiny_features do
   author_url 'https://github.com/nanego'
   requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.4' if Rails.env.test?
   requires_redmine_plugin :redmine_base_deface, :version_or_higher => '0.0.1'
+  
+  permission :always_see_user_email_addresses, {}
 
-  project_module :user_email do
-    permission :always_see_user_email_addresses ,{}
-  end
   settings partial: 'settings/redmine_plugin_tiny_features_settings',
            default: {
-               'warning_message_on_closed_issues': '1',
-               'open_issue_when_editing_closed_issues': '',
-               'simplified_version_form': '1',
-               'default_project': '',
-               'paginate_issue_filters_values': Rails.env.test? || !(Redmine::Plugin.installed?(:redmine_base_select2)) ? '0' : '1',
-               'journalize_note_deletion': Rails.env.test? ? '0' : '1',
-               'use_select2': Rails.env.test? || !(Redmine::Plugin.installed?(:redmine_base_select2)) ? '0' : '1',
-               'do_not_preload_issue_edit_form': Rails.env.test? ? '0' : '1',
-               'disable_email_hiding': '',
+             'warning_message_on_closed_issues': '1',
+             'open_issue_when_editing_closed_issues': '',
+             'simplified_version_form': '1',
+             'default_project': '',
+             'paginate_issue_filters_values': Rails.env.test? || !(Redmine::Plugin.installed?(:redmine_base_select2)) ? '0' : '1',
+             'journalize_note_deletion': Rails.env.test? ? '0' : '1',
+             'use_select2': Rails.env.test? || !(Redmine::Plugin.installed?(:redmine_base_select2)) ? '0' : '1',
+             'do_not_preload_issue_edit_form': Rails.env.test? ? '0' : '1',
+             'disable_email_hiding': '',
            }
 end
