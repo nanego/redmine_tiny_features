692f0881067351826376ef52390030629dcc348c
Vincent Robert <vincent.robert@nanego.com>
2023-02-07 17:02:46 +0100

    Fix missing JS tags

--- app/overrides/issues/_action_menu_edit.rb
+++ app/overrides/issues/_action_menu_edit.rb
@@ -1,8 +1,13 @@
 Deface::Override.new :virtual_path  => 'issues/_action_menu_edit',
-                       :name        => 'prevents-render-edit-partial-render-it-only-on-edit-button-click',
-                       :replace     => "erb[loud]:contains(\"render :partial => 'edit'\")",
-                       :text        => <<-RENDER_PARTIAL
-<% if !Setting.plugin_redmine_tiny_features.key?('do_not_preload_issue_edit_form') %>
+                     :name          => 'prevents-render-edit-partial-render-it-only-on-edit-button-click',
+                     :replace       => "erb[loud]:contains(\"render :partial => 'edit'\")",
+                     :text          => <<-RENDER_PARTIAL
+<% unless Setting.plugin_redmine_tiny_features.key?('do_not_preload_issue_edit_form') %>
   <%= render :partial => 'edit' %>
+<% else %>
+  <%# These header tags are not included if the partial is not preloaded %>
+  <% content_for :header_tags do %>
+    <%= javascript_include_tag 'attachments' %>
+  <% end %>
 <% end %>
 RENDER_PARTIAL
