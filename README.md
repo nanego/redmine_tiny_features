7286977e9ec2977c040b22c9b6ced43b6b09af10
Vincent <contact@vincent-robert.com>
2024-11-28 14:49:30 +0100

    Add Redmine 5.1.4 to CI Config & complete Readme

--- README.md
+++ README.md
@@ -27,17 +27,16 @@ Here is a complete list of the features:
 * PDF exports: add **links to attached files in generated PDF**"
 * Projects overview: add an option to **hide members section**
 * **Hide status select box** in new issue form if only one status is available
-* Add a link to quickly **create a new issue related to the current one**
 
 ## Test status
 
 | Plugin branch | Redmine Version | Test Status       |
 |---------------|-----------------|-------------------|
 | master        | 4.2.11          | [![4.2.11][1]][5] |
-| master        | 5.1.3           | [![5.1.3][2]][5]  |
+| master        | 5.1.4           | [![5.1.4][2]][5]  |
 | master        | master          | [![master][3]][5] |
 
 [1]: https://github.com/nanego/redmine_tiny_features/actions/workflows/4_2_11.yml/badge.svg
-[2]: https://github.com/nanego/redmine_tiny_features/actions/workflows/5_1_3.yml/badge.svg
+[2]: https://github.com/nanego/redmine_tiny_features/actions/workflows/5_1_4.yml/badge.svg
 [3]: https://github.com/nanego/redmine_tiny_features/actions/workflows/master.yml/badge.svg
 [5]: https://github.com/nanego/redmine_tiny_features/actions
--- app/overrides/issues/_action_menu.rb
+++ app/overrides/issues/_action_menu.rb
@@ -4,16 +4,3 @@ Deface::Override.new :virtual_path => 'issues/_action_menu',
                      :text => <<-HIDE_LINK
                       && !@issue.tracker.prevent_issue_copy
                      HIDE_LINK
-
-Deface::Override.new :virtual_path => 'issues/_action_menu',
-                     :name => 'add-link-issue-button-to-issue-actions',
-                     :insert_before => 'erb[loud]:contains("link_to l(:button_edit)")',
-                     :text => <<~RELATEDISSUE
-                       <% if Setting["plugin_redmine_tiny_features"]["create_related_issue_shortcut"].present? %>
-                           <%= link_to l(:label_create_related_issue_shortcut),
-                               new_project_issue_path(project_id: Setting["plugin_redmine_tiny_features"]["create_related_issue_shortcut_project_id"] || @project.id,
-                                                      'issue[description]' =>  @issue.description,
-                                                      related_to: @issue,),
-                               class: "icon icon-link-break" %>
-                       <% end %>
-                     RELATEDISSUE
