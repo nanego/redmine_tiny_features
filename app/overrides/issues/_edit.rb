a0bc513ff710cb0da66c9fb7b048fa5f4b26e5e5
Vincent <contact@vincent-robert.com>
2026-07-16 16:49:14 +0200

    Fix Redmine 7 compatibility: SVG icon links and textarea rename

--- app/overrides/issues/_edit.rb
+++ app/overrides/issues/_edit.rb
@@ -1,7 +1,7 @@
 #encoding: utf-8
 Deface::Override.new :virtual_path  => "issues/_edit",
                      :name          => "add-warning-above-notes-textarea-if-issue-closed",
-                     :insert_before => "erb[loud]:contains('text_area')",
+                     :insert_before => "erb[loud]:contains('wiki-edit')",
                      :partial       => "issues/edit_closed"
 
 unless Rails.env.test?
